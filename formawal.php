3d89fa88f4ddeec5b214c8721cae35e6d244b606
nikereginaputri <43513339+nikereginaputri@users.noreply.github.com>
2018-09-30 18:53:23 +0700

    Update formawal.php
    NIKEREGINA_6701174080_D3MI4104

--- formawal.php
+++ formawal.php
@@ -1,5 +1,47 @@
 <?php
+<html>
+<head>
+	<title> TA 4 webdas</title>	
+</head>
+<body>
+	<h1>LOGIN</h1>
+	
+	<form action="#" method="post">		
+		<table>
+		<tr>
+				<td>Username</td>
+				<td><input type="text" name="username"></td>
+		</tr>
+		<tr>
+				<td>Password</td>
+				<td><input type="password" name="password"></td>
+		</tr>
+		<tr>
+				<td></td>
+				<td><input type="submit" name="login" value="Log In"></td>
+		</tr>
+		</table>
+	</form>
+</body>
+</html>
 
+<?php
+	if (isset($_POST['login'])) {
+		$username=$_POST['username'];
+		$password=$_POST['password'];
+
+		if ($username=="admin"&&$password="admin") {
+		echo " LOGIN BERHASIL";
+?>
+	<a href="prosesta4.php" > LANJUT KE PROSES </a>
+	<?php  
+		}else{
+		echo "LOGIN GAGAL";
+		}
+		
+	}
+
+?>
 
 
 ?>
